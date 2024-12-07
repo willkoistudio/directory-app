fe46a30a2446b645c245f95fd5379a25d35102f1
William Koï <koi.william91@gmail.com>
2024-12-07 14:39:19 -0500

    init

---
+++ src/App.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "./App";
+
+export default {
+  title: "App",
+  component: App,
+};
+
+const Template = (args) => <App {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  // Ajoutez ici les props par défaut si nécessaire
+};
