976ba1352a9debd98ed0dde1c190aae2f0efc2ef
William KOÏ <koi.william91@gmail.com>
2026-01-28 17:27:59 -0500

    add logout

--- src/components/Navigation/Header/hooks/useDropdownMenuElements.tsx
+++ src/components/Navigation/Header/hooks/useDropdownMenuElements.tsx
@@ -1,21 +1,26 @@
 /** @format */
 
 import { useMemo, useState } from "react";
-import { Languages, Settings, SunMoon } from "lucide-react";
+import { Languages, LogOut, Settings, SunMoon } from "lucide-react";
 import { DropdownMenuItem } from "../../../ui/dropdown-menu";
 import { LANGUAGES_APP } from "../../../../const/locale";
 import i18n from "../../../../i18n";
 import { APP_THEME } from "../../../../const/theme";
 import { useTranslation } from "react-i18next";
+import { useAppDispatch } from "../../../../store/store";
+import { logout } from "../../../../store/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const useDropdownMenuElements = () => {
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
   const [currentTheme, setCurrentTheme] = useState(() => {
-    const savedTheme = localStorage.getItem('app-theme');
+    const savedTheme = localStorage.getItem("app-theme");
     return (savedTheme as APP_THEME) || APP_THEME.DARK;
   });
   const themes = [APP_THEME.LIGHT, APP_THEME.DARK];
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
@@ -27,13 +32,13 @@ const useDropdownMenuElements = () => {
       document.body.classList.remove("dark");
       document.body.classList.add("light");
       setCurrentTheme(APP_THEME.LIGHT);
-      localStorage.setItem('app-theme', APP_THEME.LIGHT);
+      localStorage.setItem("app-theme", APP_THEME.LIGHT);
       return;
     }
     document.body.classList.remove("light");
     document.body.classList.add("dark");
     setCurrentTheme(APP_THEME.DARK);
-    localStorage.setItem('app-theme', APP_THEME.DARK);
+    localStorage.setItem("app-theme", APP_THEME.DARK);
   };
 
   return useMemo(
@@ -67,16 +72,22 @@ const useDropdownMenuElements = () => {
         )),
       },
       {
-        label: "Settings",
-        icon: <Settings className="cursor-pointer hover:text-gray " />,
+        label: "Logout",
+        icon: <LogOut className="cursor-pointer hover:text-gray " />,
         content: (
-          <DropdownMenuItem className="cursor-pointer hover:bg-white/5">
+          <DropdownMenuItem
+            className="cursor-pointer hover:bg-white/5"
+            onClick={async () => {
+              await dispatch(logout());
+              navigate("/login");
+            }}
+          >
             {t("login.logout")}
           </DropdownMenuItem>
         ),
       },
     ],
-    [currentLanguage, currentTheme]
+    [currentLanguage, currentTheme, dispatch, navigate],
   );
 };
 
